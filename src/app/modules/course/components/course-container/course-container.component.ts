35acec32cac3da39e307ea111f9caed4379a71e5
randygordon040 <72591791+randygordon040@users.noreply.github.com>
2021-01-10 23:04:10 -0500

    feat: pregunta 2 B,C,D ejecicios

--- src/app/modules/course/components/course-container/course-container.component.ts
+++ src/app/modules/course/components/course-container/course-container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseContainerComponent implements OnInit {
 
+  listCurso: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/modules/course/course.module.ts
+++ src/app/modules/course/course.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CourseRoutingModule } from './course-routing.module';
 import { CoursepageComponent } from './pages/coursepage/coursepage.component';
-import { CourseComponent } from './components/course/course.component';
+// import { CourseComponent } from './components/course/course.component';
 import { CourseContainerComponent } from './components/course-container/course-container.component';
-import { InstructorComponent } from './components/instructor/instructor.component';
-import { InstructorContainerComponent } from './components/instructor-container/instructor-container.component';
+// import { InstructorComponent } from './components/instructor/instructor.component';
+// import { InstructorContainerComponent } from './components/instructor-container/instructor-container.component';
 import { CourseDetailtPageComponent } from './pages/course-detailt-page/course-detailt-page.component';
 
 
 @NgModule({
-  declarations: [CoursepageComponent, CourseComponent, CourseContainerComponent, InstructorComponent, InstructorContainerComponent, CourseDetailtPageComponent],
+  // declarations: [CoursepageComponent, CourseComponent, CourseContainerComponent, InstructorComponent, InstructorContainerComponent, CourseDetailtPageComponent],
+  declarations: [CoursepageComponent, CourseContainerComponent, CourseDetailtPageComponent, CourseCatContainerComponent],
   imports: [
     CommonModule,
-    CourseRoutingModule
+    CourseRoutingModule,
+    SharedModule 
   ]
 })
 export class CourseModule { }
